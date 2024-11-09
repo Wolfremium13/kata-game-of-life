0ab6cbaa391ddf3f318f5ce5d16e55c73016b3a1
Wolfremium13 <wolfremiuminformatica@gmail.com>
2024-11-09 10:37:12 +0000

    red: not_allow_to_add_cells_outside_of_world_dimensions

--- kata-game-of-life/WorldShould.cs
+++ kata-game-of-life/WorldShould.cs
@@ -29,6 +29,17 @@ public class WorldShould
 
         _world.IsCellAliveAt(positionWithoutCell).Should().BeFalse();
     }
+    
+    [Fact]
+    public void not_allow_to_add_cells_outside_of_world_dimensions()
+    {
+        var outsideWorldPosition = new Position(8, 8);
+        
+        Action addCellOutsideWorld = () => _world.AddCellAt(outsideWorldPosition);
+
+        addCellOutsideWorld.Should().Throw<ArgumentOutOfRangeException>()
+            .WithMessage("Cell position is outside of world dimensions");
+    }
 }
 
 public record Position(int X, int Y);
